1ee12c810c9f7df2068aeec9b5c2a6092e7b199d
ktz <kaktyzzz89@gmail.com>
2013-11-11 05:41:15 +0400

    + moving

--- src/com/example/bubble/MainActivity.java
+++ src/com/example/bubble/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.bubble;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.app.Activity;
 import android.content.Context;
 import android.graphics.Canvas;
@@ -25,6 +26,8 @@ public class MainActivity extends Activity {
 	}
 
 	public class GraphicsView extends View {
+		Handler mHandler = new Handler();
+		static final long FRAME_TIME = 300;
 		
 		public GraphicsView(Context context) {
 			super(context);
@@ -49,15 +52,21 @@ public class MainActivity extends Activity {
 		protected void onDraw(Canvas canvas) {
 			ListBall cursor;
 			cursor = listBall;
+			canvas.drawColor(Color.WHITE);
 			
 			while (cursor != null) {
 				Ball ball = cursor.getValue();
+				ball.move();
+				ball.cross(canvas);
 				
 				canvas.drawCircle(ball.centre.x, ball.centre.y, ball.r, ball.paint);
 				
 				cursor = cursor.getNext();
 			}
+			invalidate();
 		}
+		
+		
 
 	}	
 }
@@ -69,9 +78,14 @@ class Point {
 		this.x = x;
 		this.y = y;
 	}
+	
+	public void copyFrom (Point p) {
+		x = p.x;
+		y = p.y;
+	}
 }
 
-class ListBall {
+class ListBall { //связный список шаров (вместо массива)
 	private Ball value;
 	private ListBall next;
 	
@@ -97,15 +111,56 @@ class ListBall {
 }
 
 class Ball {
-	public Point centre;
-	public float r,w;
+	public Point centre, base;
+	public float r,speed;
 	public Paint paint;
+	private int angle;
 	
 	public Ball (Point p) {
 		centre = p;
-		r = (float) Math.random() * 30 + 10;
+		base = new Point (p.x, p.y);
+		r = (float) Math.random() * 40 + 10;
+		//angle = 45;
+		angle = (int) Math.round(Math.random() * 360);
+		speed = (float) Math.random() * 4 + 2;
+		//speed = 1;
 		
 		paint = new Paint ();
-		paint.setColor(Color.rgb((int) Math.round(Math.random()*255), (int) Math.round(Math.random()*255), (int) Math.round(Math.random()*255)));
+		paint.setColor(Color.rgb((int) Math.round(Math.random() * 255), (int) Math.round(Math.random() * 255), (int) Math.round(Math.random() * 255)));
 	}
+	
+	public void reduceSpeed () {
+		speed -= speed * 0.1;
+	}
+	
+	public void inverseSpeed () {
+		speed *= -1;
+	}
+	
+	public void move () {
+        centre.x += (float) Math.cos(angle * Math.PI / 180) * speed;
+        float t = (float) Math.tan (angle * Math.PI / 180);
+        centre.y = t * centre.x + (base.y - t * base.x); // уравнение прямой
+	}
+	
+	private void correct_params () {
+		reduceSpeed(); //снижаем скорость
+		inverseSpeed(); // меняем направление
+    	base.copyFrom(centre); // выравниваем абсолютные координаты
+	}
+	
+	public void cross (Canvas canvas) { // пересечение с канвой
+            if ((cross (r, 0, centre.y)) || (cross (r, canvas.getHeight(), centre.y))) {
+            	angle = 180 - angle;
+            	correct_params();
+            }
+            else if ((cross (r, 0, centre.x)) || (cross (r, canvas.getWidth(), centre.x))) {
+            	angle = 360 - angle;
+            	correct_params();
+            }
+    }
+        public Boolean cross (float r, float c0, float c1) { //пересечение окружности с прямой
+            return (r * r - Math.pow ((c0 - c1), 2)) >= 0;
+    }
+	
 }
